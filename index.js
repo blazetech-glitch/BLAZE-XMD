6e1bb1f5c0c126e4b47b70ce582f8dd756539060
blazetech-glitch <2.35096418e+08+blazetech-glitch@users.noreply.github.com>
2026-08-19 06:09:24 +0000

    Update startup website to t20tech.site

--- index.js
+++ index.js
@@ -1257,7 +1257,8 @@ client.ev.on('group-participants.update', async (group) => {
 │❒ *Prefix*: *[ ${prefixe} ]*
 
 │❒ *BLAZE XMD Official*
-│❒ https://whatsapp.com/channel/0029VbAjawl9MF8vQQa0ZT32
+│❒ *Website*: https://t20tech.site
+│❒ *Channel*: https://whatsapp.com/channel/0029VbAjawl9MF8vQQa0ZT32
 ◈━━━━━━━━━━━━━━◈`;
 
                 const ownerNum = (getConf('NUMERO_OWNER') || conf.NUMERO_OWNER || '').replace(/[^0-9]/g, '');
